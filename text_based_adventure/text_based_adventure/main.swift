98d25488ccddc19b04bfd37d8928937710a0f236
J <jevongordon@pursuit.org>
2018-10-10 15:06:59 -0400

    Initial commit

---
+++ text_based_adventure/text_based_adventure/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  text_based_adventure
+//
+//  Created by J on 10/10/18.
+//  Copyright © 2018 J. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//print(")
+//if let response = readline(){
+//    
+//}
+//
+print("Which Game would you like to play: (Rock, Paper, Scissor), (Escape the Room), (Jeopardy)")
+
+// String? - optional
+if let response = readLine() {
+    switch response {
+    case "a":
+        print("")
+    case "b":
+        print("")
+    case "c":
+        print("")
+    default:
+        print("")
+    }
+}
